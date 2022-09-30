bf98759d272229cde76e1a28c35c12eb21b435a5
ShawyYum <111735776+ShawyYum@users.noreply.github.com>
2022-09-30 17:46:08 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@ int main() {
     std::vector<int> v {9,8,7,6,5,4,3,2,1};
 
     // 4-Fork-Min-Heap
-    heap<int,std::vector<int>,4,std::greater<int>> hp(v.begin(),v.end());
+    heap<4,int,std::vector<int>,std::greater<int>> hp(v.begin(),v.end());
     std::cout << "hp.size() = " << hp.size() << "\n";
     std::cout << "hp.top() = " << hp.top() << "\n";
 
